8da0aa8f98dbd797b9f25d2b946ccb41b967b132
ANDRE <klai1@mail.ru>
2022-09-04 00:16:04 +0400

    DZ

--- src/main/java/com/MarvenAndSpringBoot/calculator/conroller/ColkulatorController.java
+++ src/main/java/com/MarvenAndSpringBoot/calculator/conroller/ColkulatorController.java
@@ -22,26 +22,29 @@ public class ColkulatorController {
 
     @GetMapping("/plus")
     public String plus(@RequestParam("num1") int a,
-                        @RequestParam("num2") int b){
-        return returnLines(a,b,colkulateoService.sum(a,b),"+");
+                       @RequestParam("num2") int b) {
+        return returnLines(a, b, colkulateoService.sum(a, b), "+");
     }
+
     @GetMapping("/minus")
     public String minus(@RequestParam("num1") int a,
-                        @RequestParam("num2") int b){
-        return returnLines(a,b,colkulateoService.minus(a,b),"-");
+                        @RequestParam("num2") int b) {
+        return returnLines(a, b, colkulateoService.minus(a, b), "-");
     }
+
     @GetMapping("/multiply")
     public String multiply(@RequestParam("num1") int a,
-                        @RequestParam("num2") int b){
-        return returnLines(a,b,colkulateoService.multiply(a,b),"*");
+                           @RequestParam("num2") int b) {
+        return returnLines(a, b, colkulateoService.multiply(a, b), "*");
     }
+
     @GetMapping("/divide")
     public String divide(@RequestParam("num1") int a,
-                        @RequestParam("num2") int b){
-        if (b==0){
+                         @RequestParam("num2") int b) {
+        if (b == 0) {
             return "На ноль делить нельзя";
         }
-        return returnLines(a,b,colkulateoService.divide(a,b),"/");
+        return returnLines(a, b, colkulateoService.divide(a, b), "/");
     }
 
     public String returnLines(int a, int b, Number result, String operation) {
